f36ab2cbec0cab720f44cbb07f06a15a8794f024
wuhuancai666 <wuhuancai666@dingtalk.com>
2022-03-19 14:24:43 +0800

    哈哈

--- hall-server/src/main/java/com/game/core/cmd/AcceptorListener.java
+++ hall-server/src/main/java/com/game/core/cmd/AcceptorListener.java
@@ -5,10 +5,12 @@ import com.game.core.manager.SpringHolder;
 import com.me.transport.Message;
 import com.me.transport.event.IOEvent;
 import com.me.transport.event.IOEventListener;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author wu_hc 【whuancai@163.com】
  */
+@Slf4j
 public final class AcceptorListener implements IOEventListener {
     @Override
     public void onEvent(IOEvent ioEvent) {
