cb2f2725a8f243a11cdeb97c477beafd68c03c28
wuhuancai666 <wuhuancai666@dingtalk.com>
2022-03-21 00:42:21 +0800

    工程目录整理

--- hall-server/src/main/java/com/me/AppBootstrap.java
+++ hall-server/src/main/java/com/me/AppBootstrap.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
 @MapperScan(value = "com.me.metadata.db.mapper")
 public class AppBootstrap {
