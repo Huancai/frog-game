d3a364bbe1db4ef1a2cec419e45df94587346ccc
wuhuancai666 <wuhuancai666@dingtalk.com>
2022-03-22 22:21:03 +0800

    废弃代码

--- frog-common/src/main/java/com/me/common/db/DBConstant.java
+++
@@ -1,7 +0,0 @@
-package com.me.common.db;
-
-public interface DBConstant {
-
-	boolean USE_BATCH = Boolean.TRUE;
-	int BATCH_NUM = 800;
-}
--- frog-common/src/main/java/com/me/common/db/Dao.java
+++
@@ -1,5 +0,0 @@
-package com.me.common.db;
-
-public final class Dao {
-
-}
